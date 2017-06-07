c9474bcd2e2f81d9140a90e28a0862f23d940bc6
ty <1093438759@qq.com>
2017-06-07 17:23:07 +0800

    增加

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ty on 2017/6/5.
+ */
+
+var config ={
+    url:"mongodb://127.0.0.1:27017/doc",
+    mongoose:"mongoose",
+    collectionName:"video",
+    discussCollection:"discuss",
+}
+
+module.exports=config;
